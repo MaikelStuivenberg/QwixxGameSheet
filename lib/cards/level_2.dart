85ee6b82a3112ed5c4730591b24d431c185d7da7
Maikel Stuivenberg <maikelstuivenberg@gmail.com>
2022-08-27 11:36:43 +0200

    add 3 levels

---
+++ lib/cards/level_2.dart
@@ -0,0 +1,66 @@
+
+import 'package:qwixx_scoreboard/constants/box_colors.dart';
+import 'package:qwixx_scoreboard/models/card_box.dart';
+
+class Level2Card {
+
+  final _row1 = [
+    CardBox(BoxColors.redBox, 10),
+    CardBox(BoxColors.redBox, 6),
+    CardBox(BoxColors.redBox, 2),
+    CardBox(BoxColors.redBox, 8),
+    CardBox(BoxColors.redBox, 3),
+    CardBox(BoxColors.redBox, 4),
+    CardBox(BoxColors.redBox, 12),
+    CardBox(BoxColors.redBox, 5),
+    CardBox(BoxColors.redBox, 9),
+    CardBox(BoxColors.redBox, 7),
+    CardBox(BoxColors.redBox, 11),
+  ];
+
+  final _row2 = [
+    CardBox(BoxColors.yellowBox, 9),
+    CardBox(BoxColors.yellowBox, 12),
+    CardBox(BoxColors.yellowBox, 4),
+    CardBox(BoxColors.yellowBox, 6),
+    CardBox(BoxColors.yellowBox, 7),
+    CardBox(BoxColors.yellowBox, 2),
+    CardBox(BoxColors.yellowBox, 5),
+    CardBox(BoxColors.yellowBox, 8),
+    CardBox(BoxColors.yellowBox, 11),
+    CardBox(BoxColors.yellowBox, 3),
+    CardBox(BoxColors.yellowBox, 10),
+  ];
+
+  final _row3 = [
+    CardBox(BoxColors.greenBox, 8),
+    CardBox(BoxColors.greenBox, 2),
+    CardBox(BoxColors.greenBox, 10),
+    CardBox(BoxColors.greenBox, 12),
+    CardBox(BoxColors.greenBox, 6),
+    CardBox(BoxColors.greenBox, 9),
+    CardBox(BoxColors.greenBox, 7),
+    CardBox(BoxColors.greenBox, 4),
+    CardBox(BoxColors.greenBox, 5),
+    CardBox(BoxColors.greenBox, 11),
+    CardBox(BoxColors.greenBox, 3),
+  ];
+
+  final _row4 = [
+    CardBox(BoxColors.blueBox, 5),
+    CardBox(BoxColors.blueBox, 7),
+    CardBox(BoxColors.blueBox, 11),
+    CardBox(BoxColors.blueBox, 9),
+    CardBox(BoxColors.blueBox, 12),
+    CardBox(BoxColors.blueBox, 3),
+    CardBox(BoxColors.blueBox, 8),
+    CardBox(BoxColors.blueBox, 10),
+    CardBox(BoxColors.blueBox, 2),
+    CardBox(BoxColors.blueBox, 6),
+    CardBox(BoxColors.blueBox, 4),
+  ];
+
+  List<List<CardBox>> getCard() {
+    return [_row1, _row2, _row3, _row4];
+  }
+}
---
+++ lib/constants/box_colors.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:qwixx_scoreboard/models/box_color.dart';
+
+class BoxColors {
+  BoxColors._();
+
+  static var redBox = BoxColor(
+      Colors.red,
+      const Color.fromRGBO(255, 255, 255, 1),
+      Colors.red,
+      const Color.fromRGBO(0, 0, 0, 1));
+
+  static var yellowBox = BoxColor(
+      const Color.fromARGB(255, 245, 200, 66),
+      const Color.fromRGBO(255, 255, 255, 1),
+      const Color.fromARGB(255, 245, 200, 66),
+      const Color.fromRGBO(0, 0, 0, 1));
+
+  static var greenBox = BoxColor(
+      const Color.fromARGB(255, 0, 125, 0),
+      const Color.fromRGBO(255, 255, 255, 1),
+      const Color.fromARGB(255, 0, 125, 0),
+      const Color.fromRGBO(0, 0, 0, 1));
+
+  static var blueBox = BoxColor(
+      const Color.fromARGB(255, 41, 72, 143),
+      const Color.fromRGBO(255, 255, 255, 1),
+      const Color.fromARGB(255, 41, 72, 143),
+      const Color.fromRGBO(0, 0, 0, 1));
+}
